88c44a3592dae402582b783aea9320b97422efaa
ADARSH RAJ <adarshsingh180745@gmail.com>
2026-09-25 22:02:55 +0530

    Add files via upload

---
+++ class Address {.java
@@ -0,0 +1,9 @@
+class Address {
+    public static void main(String[] args) {
+        System.out.println("I'm Adarsh Raj.");
+        System.out.println("I'm from Bihar.");
+        System.out.println("I'm from India.");
+    }
+    
+    
+}
